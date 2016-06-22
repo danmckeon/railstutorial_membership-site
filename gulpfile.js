78bdf147da428d9c9f26c83ad77c109e57fde7be
Clinton Campbell <clinton@quirkconcepts.com>
2016-06-21 20:59:29 -0700

    Browser syncing

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var $ = require('gulp-load-plugins')();
-var gulp         = require('gulp');
-var rimraf          = require('rimraf');
+var browser = require('browser-sync');
+var gulp = require('gulp');
+var rimraf = require('rimraf');
 var sequence = require('run-sequence');
 
-
-gulp.task('clean',function(done) {
-  //Delete our old css files
-  rimraf('static/css/**/*', done);
+gulp.task('clean', function(done) {
+    //Delete our old css files
+    rimraf('static/css/**/*', done);
 });
 
 // Compile SCSS files to CSS
-gulp.task('scss', function() {
+gulp.task('sass', function() {
     return gulp.src('assets/sass/styles.scss')
         .pipe($.sourcemaps.init())
         .pipe($.sass())
@@ -19,6 +19,18 @@ gulp.task('scss', function() {
         .pipe(gulp.dest('static/css'));
 });
 
-gulp.task('default', function(done) {
-  sequence('clean', 'scss', done);
+gulp.task('build', function(done) {
+  sequence('clean', 'sass', done);
+});
+
+// Start a server with LiveReload to preview the site in
+gulp.task('server', function() {
+  browser.init({
+    server: '.',
+    port: 8000
+  });
+});
+
+gulp.task('default', ['build', 'server'], function() {
+  gulp.watch('assets/sass/**/*', ['sass', browser.reload])
 });
